aee245496674c79230e4ef582f795c329dbfe247
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-03-21 22:34:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,53 @@
 import os
 import openai
 import streamlit as st
-import docx
+import spacy
+import tempfile
 from docx import Document
-from io import BytesIO
+from docx.shared import Pt
 
-# Configura tu clave API de OpenAI
-openai.api_key = "tu_clave_api"
+# Configura tu API key de OpenAI
+openai.api_key = "tu_api_key"
 
-# Función para procesar el texto con GPT-4 y corregir la gramática y el estilo
-def corregir_texto(texto):
-    prompt = f"Corrija la gramática y el estilo del siguiente texto:\n\n{texto}\n\nTexto corregido:"
-    completions = openai.Completion.create(
+# Carga el modelo de lenguaje de spaCy
+nlp = spacy.load("en_core_web_sm")
+
+def correct_document(doc):
+    corrected_doc = Document()
+    for paragraph in doc.paragraphs:
+        corrected_paragraph = correct_text(paragraph.text)
+        corrected_doc.add_paragraph(corrected_paragraph)
+    return corrected_doc
+
+def correct_text(text):
+    prompt = f"Please correct the following text for grammar and style: {text}"
+    response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=1024,
+        max_tokens=100,
         n=1,
         stop=None,
         temperature=0.5,
     )
+    corrected_text = response.choices[0].text.strip()
+    return corrected_text
 
-    texto_corregido = completions.choices[0].text.strip()
-    return texto_corregido
+st.title("Corrector de gramática y estilo de documentos")
 
-# Función para corregir un documento docx
-def corregir_docx(documento):
-    doc = Document(documento)
-    for paragraph in doc.paragraphs:
-        texto_original = paragraph.text
-        texto_corregido = corregir_texto(texto_original)
-        paragraph.text = texto_corregido
+uploaded_file = st.file_uploader("Sube un archivo .docx", type="docx")
 
-    output_doc = BytesIO()
-    doc.save(output_doc)
-    output_doc.seek(0)
-    return output_doc
+if uploaded_file:
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".docx") as temp_docx:
+        temp_docx.write(uploaded_file.getvalue())
+        doc = Document(temp_docx.name)
 
-# Interfaz de Streamlit
-st.set_page_config(page_title="Corrección de gramática y estilo", page_icon=None, layout="wide", initial_sidebar_state="auto")
+    corrected_doc = correct_document(doc)
 
-st.title("Corrección automática de gramática y estilo de documentos")
-st.write("Esta aplicación utiliza GPT-4 para corregir automáticamente la gramática y el estilo de tus documentos en formato .docx.")
-
-uploaded_file = st.file_uploader("Sube un archivo .docx", type=["docx"])
-
-if uploaded_file is not None:
-    if st.button("Corregir documento"):
-        with st.spinner("Corrigiendo gramática y estilo..."):
-            corrected_doc = corregir_docx(uploaded_file)
-            st.success("Documento corregido con éxito")
-            st.download_button(label="Descargar documento corregido", data=corrected_doc, file_name="documento_corregido.docx", mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".docx") as corrected_temp_docx:
+        corrected_doc.save(corrected_temp_docx.name)
+        st.download_button(
+            label="Descargar documento corregido",
+            data=corrected_temp_docx,
+            file_name="documento_corregido.docx",
+            mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        )
